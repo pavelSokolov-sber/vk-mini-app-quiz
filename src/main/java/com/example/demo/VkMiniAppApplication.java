3403349825cf4bbf9fb33fd0f05e866fc9092c09
Pavel Sokolov <pasch.sockolow2012@yandex.ru>
2021-04-21 19:39:05 +0300

    create progect

---
+++ src/main/java/com/example/demo/VkMiniAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VkMiniAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VkMiniAppApplication.class, args);
+    }
+
+}
